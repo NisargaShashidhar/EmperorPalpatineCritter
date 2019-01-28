99dac6550dccea67166db7e5321bb8577c257b8b
NisargaShashidhar <45395264+NisargaShashidhar@users.noreply.github.com>
2019-01-27 20:53:51 -0500

    Update EmperorPalpatine.java
    Fixed some errors and clarified the loc variable.

--- EmperorPalpatine.java
+++ EmperorPalpatine.java
@@ -4,57 +4,57 @@ import info.gridworld.grid.Location;
 import info.gridworld.grid.Grid;
 import java.awt.Color;
 import java.util.ArrayList;
+import info.gridworld.actor.Rock;
 
 public class EmperorPalpatine extends Critter
 {
-    public ArrayList<Actor> getActors()
-    {
-    	 Grid<Actor> gr = getGrid();
-    	ArrayList actor = new ArrayList<Actor>();
-    	Location one = getLocation();
-	Location two = one.getAdjacentLocation(getDirection());
-    	while(actor.size == 0){
-		if(gr.get(two).isValid() == true){
-			Actor neighbor = gr.get(two);
-		
-        		if(neighbor instanceof Rock){
-				return actor;
-			}
-			else if(neighbor == null){
-				one = two;
-				two = one.getAdjacentLocation(getDirection());
+	public ArrayList<Actor> getActors()
+	{
+		Grid<Actor> gr = getGrid();
+		ArrayList actor = new ArrayList<Actor>();
+		Location one = getLocation();
+		Location two = one.getAdjacentLocation(getDirection());
+		while(actor.size() == 0){
+			if(gr.get(two).isValid() == true){
+				Actor neighbor = gr.get(two);
+
+				if(neighbor instanceof Rock){
+					return actor;
+				}
+				else if(neighbor == null){
+					one = two;
+					two = one.getAdjacentLocation(getDirection());
+				}
+				else{
+					actor.add(neighbor);
+				}
 			}
 			else{
-            			actor.add(neighbor);
+				break;
 			}
 		}
+		return actor;
+	}
+
+	public void processActors(ArrayList<Actor> actors)
+	{
+
+		if(actors.size() == 0){
+
+		}
 		else{
-			break;
+			Location loc = actors[0].getLocation();
+			actors[0].removeSelfFromGrid();
+			Ash ash = new Ash();
+			ash.putSelfInGrid(gr, loc);
 		}
 	}
-	return actor;
-    }
-	
-    public void processActors(ArrayList<Actor> actors)
-    {
-    	
-    	if(actors.size == 0){
-	
+	/**
+	* Turns towards the new location as it moves.
+	*/
+	public void makeMove(Location loc)
+	{
+		setDirection(getLocation().getDirectionToward(loc));
+		super.makeMove(loc);
 	}
-        else{
-        	removeSelfFromGrid();
-        	Ash ash = new ash();
-        	ash.putSelfInGrid(gr, loc);
-        }
-    }
-
-    /**
-     * Turns towards the new location as it moves.
-     */
-    public void makeMove(Location loc)
-    {
-        setDirection(getLocation().getDirectionToward(loc));
-        super.makeMove(loc);
-    }
-    
 }
