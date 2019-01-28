234ec48ba4a021adeb0b821c9d2ceb971e242cac
NisargaShashidhar <45395264+NisargaShashidhar@users.noreply.github.com>
2019-01-27 20:36:16 -0500

    Update Ash.java
    Fixed all compile errors

--- Ash.java
+++ Ash.java
@@ -1,24 +1,27 @@
 import java.awt.Color;
-
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+import info.gridworld.grid.Grid;
+import info.gridworld.actor.Rock;
+import java.util.ArrayList;
 
 public class Ash extends Rock
 {
-
-    /**
-     * Constructs an ash that is the color of the image.
-     */
+/**
+* Constructs an ash that is the color of the image.
+*/
     public Ash()
     {
         setColor(null);
     }
-
-    /**
-     * Constructs a rock of a given color.
-     * @param rockColor the color of this rock
-     */
+/**
+* Constructs a rock of a given color.
+* @param rockColor the color of this rock
+*/
     public Ash(Color ashColor)
     {
         setColor(ashColor);
-    }
 
+    }
 }
